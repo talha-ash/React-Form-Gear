d2ce9f82c1b553efb3bb50ec8ee3d1c9138b64e0
xoronedge <ta.xit@yandex.com>
2020-03-28 19:53:22 +0000

    remove consoles and refactore code

--- src/customeHook/formReducer.js
+++ src/customeHook/formReducer.js
@@ -3,28 +3,16 @@ import {
   validateFields as checkFormValid
 } from "./Validation";
 
-const checkResult = (result, state, name) => {
-  if (!result.isValid) {
-    state[name].currentMessage =
-      state[name].constraints[result.caughtErrorValidationIndex].errorMessage;
-    state[name].error = true;
-  } else {
-    state[name].error = false;
-  }
-};
-
-const fieldChanger = (state, { name, value }) => {
+const handleFieldChange = (state, { name, value }) => {
   state[name].value = value;
-  console.log(state);
   let result = FieldValidator(state[name], value);
-  //checkResult(result, state, name);
   return { ...state, [name]: result };
 };
 
 const FormFieldReducer = (state, action) => {
   switch (action.type) {
     case "FieldChange":
-      return fieldChanger(state, action.payload);
+      return handleFieldChange(state, action.payload);
     case "FormSubmit":
       const { isValid, fields } = checkFormValid(state);
       if (isValid) {
